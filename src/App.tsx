66efc4dfd9938c8a98ed1602aa0b8b2c095319ca
Yogesh K <yogeshcodes@zohomail.in>
2023-07-29 17:55:47 +0530

    Build responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
+import { Grid,GridItem, Show } from "@chakra-ui/react"
 
-import { Button } from '@chakra-ui/react'
+
 
 function App() {
   return (
-      <Button colorScheme='blue'>Button</Button>
+      <>
+       <Grid 
+            templateAreas={{
+                base : `"navbar" "main"`,
+                lg   : `"navbar navbar" "leftside main"`
+            }}
+        >
+            <GridItem pl='2' bg='coral' area={'navbar'}>NavBar</GridItem>
+            <Show above="lg">
+                <GridItem pl='2' bg='gold' area={'leftside'}>Aside</GridItem>
+            </Show>
+            <GridItem pl='2' bg='blue' area={'main'}>Main</GridItem>
+
+        </Grid>
+      </>
   )
 }
 
