ea364d7f2ef407cc127be2f51f1d72709952b2ef
Yogesh K <yogeshcodes@zohomail.in>
2023-09-04 11:23:42 +0530

    Remove warnings by adding key to map

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,39 +1,50 @@
-import { Platform } from '@/hooks/useGames'
-import { HStack, Icon } from '@chakra-ui/react'
-import {FaWindows, FaApple, FaXbox, FaLinux, FaPlaystation, FaAndroid} from 'react-icons/fa'
-import {MdPhoneIphone} from 'react-icons/md'
-import {SiNintendo} from 'react-icons/si'
-import {BsGlobe} from 'react-icons/bs'
-import { IconType } from 'react-icons'
+import { Platform } from "@/hooks/useGames";
+import { HStack, Icon } from "@chakra-ui/react";
+import {
+  FaWindows,
+  FaApple,
+  FaXbox,
+  FaLinux,
+  FaPlaystation,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { IconType } from "react-icons";
 
-interface Props{
-    platforms : Platform[]
+interface Props {
+  platforms: Platform[];
 }
 
-const PlatformIconList = ({platforms}: Props) => {
-    const iconMap: {[key: string]: IconType} = {
-        pc : FaWindows,
-        playstation3 : FaPlaystation,
-        playstation4 : FaPlaystation,
-        playstation5 : FaPlaystation,
-        xbox : FaXbox,
-        xbox360: FaXbox,
-        nintendo : SiNintendo,
-        "nintendo-switch" : SiNintendo,
-        android : FaAndroid,
-        macos : FaApple,
-        ios : MdPhoneIphone,
-        linux : FaLinux,
-        web : BsGlobe,
-    }
+const PlatformIconList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation3: FaPlaystation,
+    playstation4: FaPlaystation,
+    playstation5: FaPlaystation,
+    xbox: FaXbox,
+    xbox360: FaXbox,
+    nintendo: SiNintendo,
+    "nintendo-switch": SiNintendo,
+    android: FaAndroid,
+    macos: FaApple,
+    ios: MdPhoneIphone,
+    linux: FaLinux,
+    web: BsGlobe,
+  };
 
-    return (
-        <HStack marginY={1}>
-            {platforms.map((platform) => (
-                    <Icon as={iconMap[platform.slug]} color={'gray.500'}/>
-            ))}
-        </HStack>
-    )
-}
+  return (
+    <HStack marginY={1}>
+      {platforms.map((platform) => (
+        <Icon
+          key={platform.id}
+          as={iconMap[platform.slug]}
+          color={"gray.500"}
+        />
+      ))}
+    </HStack>
+  );
+};
 
-export default PlatformIconList
+export default PlatformIconList;
