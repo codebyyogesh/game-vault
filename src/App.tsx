bf72e0f9fc3ca8be7e8083751a328298f737dd22
Yogesh K <yogeshcodes@zohomail.in>
2023-10-02 21:11:40 +0530

    Implement searching for search navbar

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
   sortOrder: string;
+  searchText: string;
 }
 function App() {
   const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
@@ -26,7 +27,11 @@ function App() {
         templateColumns={{ base: "1fr", md: "200px 1fr" }}
       >
         <GridItem pl="2" area={"navbar"}>
-          <NavBar />
+          <NavBar
+            onSearch={(text) =>
+              setGameQuery({ ...gameQuery, searchText: text })
+            }
+          />
         </GridItem>
         <Show above="md">
           <GridItem area={"leftside"} paddingX={5}>
